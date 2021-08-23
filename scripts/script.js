b0230f1619063cf33a05c66327d80bb760fdfd59
Daniel Lima <danielmlimag@gmail.com>
2021-08-23 14:41:30 -0300

    feat: add delete button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,16 @@
-function Book(title, author, pages, status) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.status = status;
-	this.info = () => {
-		return `${title} by ${author}, ${pages} pages, ${status}`;
-	};
-}
+// function Book(title, author, pages, status) {
+// 	this.title = title;
+// 	this.author = author;
+// 	this.pages = pages;
+// 	this.status = status;
+// 	this.info = () => {
+// 		return `${title} by ${author}, ${pages} pages, ${status}`;
+// 	};
+// }
 
-function addBookToLibrary() { }
+// function addBookToLibrary(book) {
+
+// }
 
 // function displayBooks() {
 // 	for (book in myLibrary) {
@@ -16,36 +18,42 @@ function addBookToLibrary() { }
 // 	}
 // }
 
-let myLibrary = [];
+// let myLibrary = [];
 
 const table = document.querySelector('#table');
 const tbody = document.querySelector('#table > tbody');
 const newBookButton = document.querySelector('#new-book-button');
+// let counter = 0;
 
 newBookButton.addEventListener('click', (e) => {
 	const tableRow = document.createElement('tr');
+	// tableRow.dataset.bookNumber = `${counter}`;
 	const bookCell = document.createElement('td');
-	bookCell.className = 'bookCell';
+	bookCell.className = 'book-cell';
 	const bookInput = document.createElement('input');
 	const authorCell = document.createElement('td');
-	authorCell.className = 'authorCell';
+	authorCell.className = 'author-cell';
 	const authorInput = document.createElement('input');
 	const pagesCell = document.createElement('td');
-	pagesCell.className = 'pagesCell';
+	pagesCell.className = 'pages-cell';
 	const pagesInput = document.createElement('input');
 	const statusCell = document.createElement('td');
-	statusCell.className = 'statusCell';
+	statusCell.className = 'status-cell';
 	const statusInput = document.createElement('input');
 	statusInput.type = 'checkbox';
-	statusInput.id = 'statusInput';
+	statusInput.id = 'status-input';
 	const statusLabel = document.createElement('label');
-	statusLabel.htmlFor = 'statusInput';
+	statusLabel.htmlFor = 'status-input';
 	statusLabel.textContent = 'Read';
 	const deleteCell = document.createElement('td');
 	const deleteButton = document.createElement('button');
 	deleteButton.className = 'delete-button';
 	const deleteButtonImage = document.createElement('img');
 	deleteButtonImage.src = './images/trash-can3.png';
+
+	deleteButton.addEventListener('click', e => {
+		tableRow.remove();
+	})
 
 	tbody.append(tableRow);
 	tableRow.append(bookCell, authorCell, pagesCell, statusCell, deleteCell);
@@ -55,6 +63,8 @@ newBookButton.addEventListener('click', (e) => {
 	statusCell.append(statusLabel, statusInput);
 	deleteCell.append(deleteButton);
 	deleteButton.append(deleteButtonImage);
+
+	counter++;
 });
 
 document.addEventListener('keydown', (e) => {
@@ -66,10 +76,3 @@ document.addEventListener('keydown', (e) => {
 		currentCell.append(inputedTextParagraph);
 	}
 });
-
-/*
-TO-DO LIST:
-- Change status button CHECK
-- Add delete button CHECK
-- Review TOP project orientation
-*/
